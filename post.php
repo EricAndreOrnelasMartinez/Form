89069ec616b6da512a1db4321ee652b92e972ca7
ericcot2@gamil.com <ericcot2@gamil.com>
2020-12-30 08:34:36 -0600

    add

--- post.php
+++ post.php
@@ -3,7 +3,7 @@ $user = $_POST['user'];
 $pass = $_POST['pass'];
 
 if($user === '' || $pass === ''){
-    echo json_encode('Faltan datos');
+    echo json_encode(1);
 }else{
-    echo json_encode('Valido usuario'.$user.'password'.$pass);
+    echo json_encode(2);
 }
