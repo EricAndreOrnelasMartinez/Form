f88f07fa57c0f2639fd1be6f950cea6b8102bc57
ericcot2@gamil.com <ericcot2@gamil.com>
2020-12-30 14:36:52 -0600

    add

--- class.php
+++ class.php
@@ -6,9 +6,9 @@ fuction conectar(){
     $password = 'Lasric.2018';
     $db = 'Minmer';
 
-    $con = mysql_connect($server, $user, $password) or die("error :(");
+    $con = mysql_connect($server, $user, $password) or die("error");
     mysql_select_db($db, $con);
 
-    return $con
+    return $con;
 
 }
