fff74b978e16d096f8e135263ef40d177ed3e00b
ericcot2@gamil.com <ericcot2@gamil.com>
2020-12-30 20:11:00 -0600

    add

--- post.php
+++ post.php
@@ -3,13 +3,13 @@ $user = $_POST['user'];
 $pass = $_POST['pass'];
 
 if($user === '' || $pass === ''){
-    json_encode('{
+    echo json_encode('{
         "contenido": {
             "title": "invalid"
         }
     }');
 }else{
-    json_encode('{
+    echo json_encode('{
         "contenido": {
             "title": "valid"
         }
