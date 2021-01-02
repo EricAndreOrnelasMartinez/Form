7f92ce10233f1e8102434a3ece8e3f5238af051d
ericcot2@gamil.com <ericcot2@gamil.com>
2021-01-01 20:24:02 -0600

    add

--- mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('class.php');
-
-$con = conectar();
-
-echo "CONECTADO!!";
-
---
+++ reg.php
@@ -0,0 +1,10 @@
+<?php
+
+include("db.php");
+
+if($con){
+    echo "todo correcto";
+}else {
+    echo "fallo";
+}
+?>
