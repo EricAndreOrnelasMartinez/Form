011d520b94e6295e8e713ca072e583b9d622a1e1
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-03 13:54:19 -0600

    add

--- xlsxread.js
+++ xlsxread.js
@@ -4,9 +4,9 @@ function read(path){
     const workbook = XLSX.readFile(path);
     const workbookSheets = workbook.SheetNames;
     const sheet = workbookSheets[0];
-    const datxlsx = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
-    for(const item of datxlsx){
-        console.log(item['Nombre']);
+    const dataxlsx = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+    for(const itemFila of dataxlsx){
+        console.log(itemFila['Nombre']);
     }
 }
 
