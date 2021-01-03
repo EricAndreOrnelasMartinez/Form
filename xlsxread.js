71c040fed9ab0a17875da5cca02b6902c61659bd
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-03 13:39:15 -0600

    add

--- xlsxread.js
+++ xlsxread.js
@@ -3,8 +3,11 @@ const XLSX = require('xlsx');
 function read(path){
     const workbook = XLSX.readFile(path);
     const workbookSheets = workbook.SheetNames;
-
-    console.log(workbookSheets);
+    const sheet = workbookSheets[0];
+    const datxlsx = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+    for(const item of datxlsx){
+        console.log(item['Nombre']);
+    }
 }
 
 read('uploads/registro1.xlsx');
