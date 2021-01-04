0da51ba7ca51c7bcc3bbb1514fee7586aea872d3
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-04 08:46:40 -0600

    add

--- xlsxread.js
+++ xlsxread.js
@@ -25,7 +25,7 @@ function read(path){
     while(i < 20){
 
         for(const itemF of dataxlsx){
-            console.log(i);
+            console.log(dataxlsx[0]);
             nombre = itemF['Nombre']
             mail = itemF['Mail']
             //console.log(dataxlsx[i])
