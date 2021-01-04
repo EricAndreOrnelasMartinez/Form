c34ac0eb89b2e1661dc8a79cd693f7c06a2ff652
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-04 12:38:32 -0600

    add

--- reg.php
+++ reg.php
@@ -35,7 +35,7 @@ if(isset($_POST['reg'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos.css">
-    <script src="xlsxread.js"></script>
+    <script src="frontend.js"></script>
     <title>Validado</title>
 </head>
 <body>
