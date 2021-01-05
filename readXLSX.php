8ab7452c92153321be3dfbfdfcc5e354d88fdf14
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-04 20:27:18 -0600

    add

--- readXLSX.php
+++ readXLSX.php
@@ -7,7 +7,7 @@ echo "todo bien2";
 
 $file  = __DIR__.'/uploads/registro4.xlsx';
 
-$obreader = PHPEXCEL_IOFactory::load($file);
+$obreader = PHPExcel_IOFactory::load($file);
 echo "todo bien 2.5";
 $obreader->setActiveShetIndex(0);
 $nRows = $obreader->setActiveSheetIndex(0)->getHighestRow();
