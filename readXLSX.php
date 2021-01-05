5015f41aa2bdf1664e15e14d0bf7ed7fe0b9de82
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-04 20:40:35 -0600

    add

--- readXLSX.php
+++ readXLSX.php
@@ -5,7 +5,7 @@ echo "todo bien .5";
 require 'Classes/PHPExcel/IOFactory.php';
 echo "todo bien2";
 
-$file  = './uploads/registro4.xlsx';
+$file  = 'uploads/registro4.xlsx';
 
 $obreader = PHPExcel_IOFactory::load($file);
 echo "todo bien 2.5";
