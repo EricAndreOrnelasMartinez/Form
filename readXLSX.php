4b739848cffb02c70ff88590ea1f5e83aa4e7e8b
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-05 10:29:44 -0600

    add

--- readXLSX.php
+++ readXLSX.php
@@ -12,13 +12,13 @@ $inputFileType = PHPExcel_IOFactory::identify($fileName);
 $obReader = PHPExcel_IOFactory::createReader($inputFileType);
 $obPHPExcel = $obReader->load($fileName); 
 echo "todo bien 2.5";
-$obreader->setActiveShetIndex(0);
-$nRows = $obreader->setActiveSheetIndex(0)->getHighestRow();
+$obPHPExcel->setActiveShetIndex(0);
+$nRows = $obPHPExcel->setActiveSheetIndex(0)->getHighestRow();
 echo "todo bien 3";
 for($i = 1; $i <= $nRows; $i++){
     echo "bien 4";
-    $nameP = $obreader->getActiveSeet()->getCell('A'.$i)->getCalculatedValue();
-    $Email = $obreader->getActiveSeet()->getCell('B'.$i)->getCalculatedValue();
+    $nameP = $obPHPExcel->getActiveSeet()->getCell('A'.$i)->getCalculatedValue();
+    $Email = $obPHPExcel->getActiveSeet()->getCell('B'.$i)->getCalculatedValue();
     echo "bien 5";
     $sql = "INSERT INTO Info(Nombre,Email) VALUES('$nameP','$Email')";
     $rmysql = $con->query($sql);
